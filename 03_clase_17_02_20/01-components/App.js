d5ec3738c5f3dc4976439384de50bdeba5606d29
Juan Zabala <juanzabaladev@gmail.com>
2020-03-02 19:12:01 -0500

    Layout examples

--- 03_clase_17_02_20/01-components/App.js
+++ 03_clase_17_02_20/01-components/App.js
@@ -5,16 +5,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.box1}>
-        <Text style={styles.text}>Vista 1</Text>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
       </View>
       <View style={styles.box2}>
-        <Text style={styles.text}>Vista 2</Text>
+        <View style={styles.box5}></View>
+        <View style={styles.box6}></View>
       </View>
-      <View style={styles.box3}>
-        <Text style={styles.text}>Vista 3</Text>
-      </View>
-
-
     </View>
   );
 }
@@ -29,15 +26,29 @@ const styles = StyleSheet.create({
     color: '#ff1744'
   },
   box1: {
-    flex: 2,
-    backgroundColor: '#ffc107'
+    flex: 1,
+    backgroundColor: '#ffc107',
+    flexDirection: 'row'
   },
   box2: {
-    flex: 2,
-    backgroundColor: '#0d47a1'
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row'
   },
   box3: {
     flex: 1,
+    backgroundColor: '#0d47a1'
+  },
+  box4: {
+    flex: 1,
     backgroundColor: '#d50000'
+  },
+  box5: {
+    flex: 1,
+    backgroundColor: '#673ab7'
+  },
+  box6: {
+    flex: 1,
+    backgroundColor: '#00bcd4'
   }
 });
