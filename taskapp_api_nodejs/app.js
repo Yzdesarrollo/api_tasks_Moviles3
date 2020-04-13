ac4afe2d6677a711f1212ea3c57068a320c1c0f1
zabala <juanzabaladev@gmail.com>
2020-04-12 19:17:41 -0500

    added nodejs api to make a taskapp

---
+++ taskapp_api_nodejs/app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import router from './Routes/ProjectRoutes';
+
+let app = express();
+app.use(express.urlencoded({extended:false}));
+app.use('/api',router);
+module.exports = app;
